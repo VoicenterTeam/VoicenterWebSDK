c534fbce257ab5af5e947e001f02edffa50e6793
Dmitry Sam <dmitry.sam88@gmail.com>
2021-03-17 15:09:14 +0200

    Change parse popup approve logick

--- libs/PopupApproveRequest.js
+++ libs/PopupApproveRequest.js
@@ -25,7 +25,7 @@ module.exports = class PopupApproveRequest extends PopupRequest {
 
     async ParseJWTData() {
         let jwtData = jwt.verify(this.request.query.data, jwtKey);
-        this = { ...this, ...jwtData };
+        Object.assign(this, jwtData);
     }
 
     Done() {
