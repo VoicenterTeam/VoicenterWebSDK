41822959ba9f727244fc1027aa81438510769477
Dmitry Sam <dmitry.sam88@gmail.com>
2021-03-19 13:31:05 +0200

    default popup logic

--- DefualtPopupLogic.js
+++ DefualtPopupLogic.js
@@ -1,10 +1,11 @@
 module.exports = async function (popUp) {
   console.debug(popUp);
+
   popUp.Result.STATUS = "OK";
   popUp.Result.URL = "https://www.google.com/search?q=" + this.phone;
+  popUp.Result.COMPANY = "Voicenter Dev Team";
   popUp.Result.CLIENTNAME = "Israel Israeli";
   popUp.Result.TOTAL = 1;
-  popUp.Result.COMPANY = "Voicenter Dev Team";
 
   popUp.Done();
 }
