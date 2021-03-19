c1915ad17ed34dbcbe4e5e9fd83336af1df5abb1
Dmitry Sam <dmitry.sam88@gmail.com>
2021-03-19 12:32:55 +0200

    Add ; symbol in end of logic

--- ServerDemo.js
+++ ServerDemo.js
@@ -1,3 +1,3 @@
-const VoicenterWebSDK = require("./index")
+const VoicenterWebSDK = require("./index");
 const vcSDK = new VoicenterWebSDK({ host: '0.0.0.0', port: 3000 });
-vcSDK.start()
+vcSDK.start();
