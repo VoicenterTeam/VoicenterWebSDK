5c635df8606b5b29e4a8cd78ae3b914460afd88d
Dmitry Sam <dmitry.sam88@gmail.com>
2021-03-22 10:21:17 +0200

    Revert "parse array in cdr ivr"
    This reverts commit 76b2b06a2946b5caae41b4b307f4cbb1d339b214.

--- libs/CdrRequest.js
+++ libs/CdrRequest.js
@@ -50,12 +50,8 @@ module.exports = class CdrRequest extends Request {
 
         if (this.request.body.IVR) {
           try {
-            if (typeof(this.request.body.IVR) === 'string') {
-              let ivrArray = JSON.parse(this.request.body.IVR);
-              if (ivrArray.constructor.name === "Array") this.IVR = ivrArray;
-            } else if(Array.isArray(this.request.body.IVR)) {
-              this.IVR = this.request.body.IVR;
-            }            
+            let ivrArray = JSON.parse(this.request.body.IVR);
+            if (ivrArray.constructor.name === "Array") this.IVR = ivrArray;
           } catch (e) {
             console.error("Failed parse IVR list of cdr ", e);
             console.error("IVR list", this.request.body.IVR);
