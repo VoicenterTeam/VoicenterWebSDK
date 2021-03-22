3c49d820209092d415728d6a85be850e94f7fd83
Dmitry Sam <dmitry.sam88@gmail.com>
2021-03-22 16:11:04 +0200

    remove key config

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const path = require('path');
 module.exports = class VoicenterWebSDK {
   constructor(options = {}) {
     this.options = options;
-    let keyConfig = {};
 
     this.config = {
       port: 3000,
