573e108bdb3e7024f2be1f1c0017de05f46ae8e2
tami.av <tami.av@voicenter.com>
2022-04-17 14:30:47 +0300

    Default logic - throw error "no logic"

--- logic/DefualtCallLogic.js
+++ logic/DefualtCallLogic.js
@@ -1,8 +1,9 @@
-module.exports = async function (call) {
-  call.SetNextLayer(0);
-  //  call.Say([{"Number" : "112"}])
-  //  call.Say({sayData:{"Number" : "113"},NextLayer:13 })
-  //call.Say(15155)
-  call.Dial("0523574321");
-  call.Execute();
-}
+module.exports = async  function (call) {
+    call.SetNextLayer(0);
+ //  call.Say([{"Number" : "112"}])
+ //  call.Say({sayData:{"Number" : "113"},NextLayer:13 })
+        //call.Say(15155)
+   // call.Dial("0523574321");
+   // call.Execute();
+   throw 'no logic is configure';
+};
